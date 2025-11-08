31e8e7e36458c189f2fd0562700d7ea190155615
Aditya <adii238@yahoo.com>
2025-11-08 14:31:53 +0530

    updated main & varibale.tf and defined a new resource group

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ provider "azurerm" {
 # ──────────────────────────────────────────────
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-demo1" # 🔸 Change this to your desired RG name
-  location = "East US"            # 🔸 Or use your preferred Azure region
+  name     = resource_group_name # Change this to your desired RG name
+  location = "East US"           # 🔸 Or use your preferred Azure region
   tags = {
     environment = "dev"
     created_by  = "Terraform Cloud"
@@ -36,13 +36,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet1"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
-  resource_group_name = "rg-terraform-demo1"
+  resource_group_name = resource_group_name
 }
 
 # Subnet 1
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
-  resource_group_name  = "rg-terraform-demo1"
+  resource_group_name  = resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -50,7 +50,7 @@ resource "azurerm_subnet" "subnet1" {
 # Subnet 2
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
-  resource_group_name  = "rg-terraform-demo1"
+  resource_group_name  = resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
